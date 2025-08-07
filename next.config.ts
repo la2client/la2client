7c45a891fc77631719e7e3b5e652255d24ae7d5b
Qinastha <qinastha@gmail.com>
2025-08-08 02:33:46 +0300

    temp fix v3.0

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: '',
+                hostname: '**',
                 port: '',
             },
         ],
